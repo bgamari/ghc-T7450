f1bb94eabdf8dd3e0998e8b70a61a685ffb92dbd
Ben Gamari <bgamari.foss@gmail.com>
2015-06-22 17:43:40 -0400

    Record GC metrics

--- test.py
+++ test.py
@@ -3,6 +3,7 @@
 import sys
 from time import time
 import subprocess
+import re
 
 def gen_program(n):
     field = lambda i: 'F%d { fldF%d :: ()}' % (i,i)
@@ -14,16 +15,24 @@ def gen_program(n):
          '  deriving (Read)'
     return s
 
-for n in [4000]:
+for n in range(2, 13):
     with open('Mod.hs', 'w') as f:
-        f.write(gen_program(n))
+        f.write(gen_program(2**n))
 
-    for i in range(10):
+    for i in range(4):
         start = time()
-        subprocess.check_call(['/opt/exp/ghc/ghc/inplace/bin/ghc-stage2', '-O', '-fforce-recomp', 'Mod.hs'],
+        subprocess.check_call(['ghc', '-O', '-fforce-recomp', 'Mod.hs', '+RTS', '-sstderr.log'],
                               stdout=sys.stderr)
         end = time()
-        print n, end-start
-        sys.stdout.flush()
+        try:
+            err = open('stderr.log').read()
+            #print err
+            alloc = int(re.search('([0-9,]+) bytes allocated in the heap', err).group(1).replace(',', ''))
+            copied = int(re.search('([0-9,]+) bytes copied during GC', err).group(1).replace(',', ''))
+
+            print n, end-start, alloc, copied
+            sys.stdout.flush()
+        except:
+            pass
 
     print
