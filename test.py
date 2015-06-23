24da000aae23eb8cbd4cec52015c64fa978de738
Ben Gamari <ben@smart-cactus.org>
2015-06-23 15:27:07 +0200

    Rework test

--- test.py
+++ test.py
@@ -15,24 +15,28 @@ def gen_program(n):
          '  deriving (Read)'
     return s
 
-for n in range(2, 13):
+if True:
     with open('Mod.hs', 'w') as f:
-        f.write(gen_program(2**n))
+        f.write(gen_program(int(sys.argv[1])))
+else:
+    for n in range(2, 13):
+        with open('Mod.hs', 'w') as f:
+            f.write(gen_program(2**n))
 
-    for i in range(4):
-        start = time()
-        subprocess.check_call(['ghc', '-O', '-fforce-recomp', 'Mod.hs', '+RTS', '-sstderr.log'],
-                              stdout=sys.stderr)
-        end = time()
-        try:
-            err = open('stderr.log').read()
-            #print err
-            alloc = int(re.search('([0-9,]+) bytes allocated in the heap', err).group(1).replace(',', ''))
-            copied = int(re.search('([0-9,]+) bytes copied during GC', err).group(1).replace(',', ''))
+        for i in range(4):
+            start = time()
+            subprocess.check_call(['ghc', '-O', '-fforce-recomp', 'Mod.hs', '+RTS', '-sstderr.log'],
+                                  stdout=sys.stderr)
+            end = time()
+            try:
+                err = open('stderr.log').read()
+                #print err
+                alloc = int(re.search('([0-9,]+) bytes allocated in the heap', err).group(1).replace(',', ''))
+                copied = int(re.search('([0-9,]+) bytes copied during GC', err).group(1).replace(',', ''))
 
-            print n, end-start, alloc, copied
-            sys.stdout.flush()
-        except:
-            pass
+                print n, end-start, alloc, copied
+                sys.stdout.flush()
+            except:
+                pass
 
-    print
+        print
